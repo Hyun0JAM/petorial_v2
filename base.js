45e0f177d961abeef1d009fdec22052f31722324
daiunii <daiunii@naver.com>
2019-07-23 15:15:35 +0900

    distribute header

--- base.js
+++ base.js
@@ -9,6 +9,33 @@ $(window).scroll(function(){
     }
 });
 $(document).ready(function(){
+    $("#regist-view").hide();$("#forgot-view").hide();
+    $(".go-login-btn").click(function(){$(".frm-view").hide();$("#login-view").show();});
+    $(".go-regist-btn").click(function(){$(".frm-view").hide(); $("#regist-view").show();});
+    $(".go-forgot-btn").click(function(){$(".frm-view").hide();$("#forgot-view").show();});
+    $(".login-btn").click(function(){
+        if($("#lg_userid").val()==""){
+            alert("아이디를 입력해 주세요.");
+            return false;
+        }
+        else if($("#lg_password").val()==""){
+            alert("암호를 입력해 주세요.");
+            return false;
+        }
+        else if($("#lg_userid").val()=="daiunii" && $("#lg_password")=="a1234"){
+            alert("Login success");
+        }
+        else alert("You can not access this page");
+    });
+    $(".profile_img").click(function(){
+        $(".profile_img_input").click();
+    });
+    $(".regist-submit-btn").click(function(){
+        if(!$("#reg_agree").is(":checked")){
+            alert("agree check false");
+            return false;
+        }
+    });
     //$("#testDiv").load("header.html");
     $(".profile").click(function(){
         location.href="myPage.html";
